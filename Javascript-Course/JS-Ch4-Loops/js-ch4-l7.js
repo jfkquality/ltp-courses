3c48fd7f4a2db47ed281aa97681cefeff00b796f
JohnK <jfkquality@gmail.com>
2018-01-12 08:16:14 -0500

    JS Simple Interest Calculator: Tweaking the layout

--- Javascript-Course/JS-Ch4-Loops/js-ch4-l7.js
+++ Javascript-Course/JS-Ch4-Loops/js-ch4-l7.js
@@ -8,15 +8,19 @@ function calcInterest(e) {
     let principal = +document.getElementById('principal').value;
     let months = +document.getElementById('periods').value;
     let APR = +document.getElementById('rate').value;
+    if (APR < 0 || APR > 100) { APR = 0; }
     let intPerMonth = APR / 12 / 100
     out = document.getElementById('result').innerHTML;
-    out += "<table>"
-    out += "<tr><th>Month</th><th>Intereest</th><th>Balance</th></tr>";
+    out += "<table><tbody>"
+    out += "<thead><tr><th>Month</th><th>Intereest</th> <th>Balance</th></tr></thead>";
+    out += "<tbody>";
+    interestAmt = principal * intPerMonth;
     for (let i = 1; i <= months; i++) {
-        interestAmt = principal * intPerMonth * i;
-        newBalance = principal + interestAmt;
+        newBalance = principal + (interestAmt * i);
         out += "<tr><td>" + i + "</td><td>" + interestAmt + "</td><td>" + newBalance + "</td></tr>" ;
     }
+    out += "</tbody>";
+    out +="<tfoot><tr><td>Total</td><td>" + (principal * intPerMonth * months) + "</td><td>" + newBalance + "</td></tr></tfoot>";
     out += "</table>";
     document.getElementById('result').innerHTML = out;
     // document.write(out);
