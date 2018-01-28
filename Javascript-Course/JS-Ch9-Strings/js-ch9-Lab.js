130362a9123190f1ed7556d4a00abb819f83ed99
JohnK <jfkquality@gmail.com>
2018-01-28 11:35:59 -0500

    JS Lab 9: sorted original string and capitalized string

--- Javascript-Course/JS-Ch9-Strings/js-ch9-Lab.js
+++ Javascript-Course/JS-Ch9-Strings/js-ch9-Lab.js
@@ -2,16 +2,26 @@ window.onload = function() {
     let myString = "the quick brown fox jumped over the lazy dog.";
 
     let out = "";
-    out += "<h1>Capitize First Letter of a String</h1>";   
+    out += "<h1>Javascript Ch. 9 Lab - Capitize First Letter of a String</h1>";   
     out += "String: " + myString;
-    out += "<br>" + myString.replace(myString.charAt(0), myString.charAt(0).toUpperCase());
+    let capString = myString.replace(myString.charAt(0), myString.charAt(0).toUpperCase());
+    out += "<br>" + capString;
     out += "<br>But it didn't change the original string: " + myString;
     out += "<br>Slice first letter: " + myString.slice(1);
     out += "<h1>Sorting the split string</h1>";
-    out +=  myString.split(" ").sort();
+    out +=  capString.split(" ").sort();
+
+    out += "<br><br>Original String"
     for (let x = 0; x < myString.split(" ").length; x++) { 
         out += "<br>" + x + ": " + myString.split(" ").sort()[x];
     }
+    out += "<br><br>Capitalized first letter";
+    for (let x = 0; x < capString.split(" ").length; x++) { 
+        out += "<br>" + x + ": " + capString.split(" ").sort()[x];
+    }
+    out += "<br>Original string: " + myString;
+    out += "<br>Uppercase string: " + myString.toUpperCase();
+    out += "<br>Original string: " + myString;
 
     document.getElementById('result').innerHTML = out;
 }
--- Javascript-Course/JS-Ch9-Strings/js-ch9-lessons.js
+++ Javascript-Course/JS-Ch9-Strings/js-ch9-lessons.js
@@ -1,11 +1,11 @@
 window.onload = function() {
     let myString = "The quick brown fox jumped over the lazy dog.";
     // alert(myString.charAt(0));
-    let out = "<h1>String Methods</h1>";
+    let out = "<h1>Javascript Ch. 9 - String Methods</h1>";
     out += myString;
     
-    out += "<br><h1>charAt(), includes(), indexOf()</h1>";
-    out += "<br>charAt 0 = " + myString.charAt(0);
+    out += "<h1>Lesson 1 - charAt(), includes(), indexOf()</h1>";
+    out += "charAt 0 = " + myString.charAt(0);
     out += "<br>includes space = " + myString.includes(" ");
     out += "<br>indexOf space = " + myString.indexOf(" ");
 
@@ -36,28 +36,28 @@ window.onload = function() {
         out += "<br>" + splitString[x];
     }
 
-    out += "<br><h1>search(), replace()</h1>";
-    out += "<br>search for lazy " + myString.search("lazy");
+    out += "<h1>Lesson2 - search(), replace()</h1>";
+    out += "search for lazy " + myString.search("lazy");
     out += "<br>replace quick with slow in myString. " + myString.replace("quick", "slow");
     out += "<br>myString = " + myString;
-    out += "<br><h3>replace() does NOT change the original string.</h3>"
+    out += "<h3>replace() does NOT change the original string.</h3>"
 
-    out += "<br><h1>slice(), split(), substr()</h1>";
-    out += "<br>slice 0-15 " + myString.slice(0, 15);
+    out += "<h1>Lesson 3 - slice(), split(), substr()</h1>";
+    out += "slice 0-15 " + myString.slice(0, 15);
     out += "<br> myString: " + myString;
     
-    out += "<br><h3>Split()</h3>";
+    out += "<h3>Split()</h3>";
     let csv = "One, Two, Three, Four, Five, Six, Seven";
     let csvArray = csv.split(",");
-    out += "<br>String: " + csv;
+    out += "String: " + csv;
     out += "<br>Split string array: " + csvArray;
     out += "<br>Index   Value";
     for (let x = 0; x < csvArray.length; x++) {
         out += "<br>" + x + ": " + csvArray[x];
     }
 
-    out += "<br><h3>substring()</h3>";
-    out += "<br>" + myString.substr(10,20);
+    out += "<h3>substring()</h3>";
+    out += myString.substr(10,20);
 
     document.getElementById('result').innerHTML = out;0
 }
