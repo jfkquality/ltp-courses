f9d5465980a2f58ddc4552b83156dff3d8f14cfb
JFK <jfkquality@gmail.com>
2018-02-25 16:26:23 -0500

    Refactor: Output childNodes to html

--- Javascript-Course/JS-Ch12-DOM/JS-Ch12-l4.js
+++ Javascript-Course/JS-Ch12-DOM/JS-Ch12-l4.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     document.title = "Find DOM Elements";
-    // let out = "<h1>" + document.title + "</h1>";
+    let out = "<h1>" + document.title + "</h1>";
     
     let result = document.getElementById('result');
     let elements = document.getElementsByTagName('p');
@@ -8,6 +8,17 @@ window.onload = function() {
     console.log(elements);
     console.log(classes);
 
+    out = "";
+    console.log(elements.length);
+    console.log(elements.hasChildNodes());
+    // if (elements.hasChildNodes) {
+    //     for (let x = 0; x < elements.hasChildNodes.length; x++) {
+    //         out += "<p> Child " + elements.childNode[x] + "</p>";
+    //     }
+    // }
+    console.log(out);
+    // out = elements
+
     // out += "<br>" + "";
-    document.getElementById('result').innerHTML = elements;
+    document.getElementById('result').innerHTML = out;
 }
